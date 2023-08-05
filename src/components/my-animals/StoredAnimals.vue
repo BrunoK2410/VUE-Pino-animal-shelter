c65c1ec087a743623a937045dd2cdffa7c2946c6
BrunoK2410 <brunokatic2410@gmail.com>
2023-08-05 22:52:09 +0200

    Added scroll to top button.

--- src/components/my-animals/StoredAnimals.vue
+++ src/components/my-animals/StoredAnimals.vue
@@ -10,11 +10,19 @@
       :breed="pet.breed"
     ></the-animal>
   </div>
+  <button :class="{ show: isShown }" @click="backToTop">
+    <font-awesome-icon icon="fa-solid fa-arrow-up" size="xl" beat />
+  </button>
 </template>
 
 <script>
 import TheAnimal from "./TheAnimal.vue";
 export default {
+  data() {
+    return {
+      isShown: false,
+    };
+  },
   computed: {
     pets() {
       if (this.$route.path === "/dog") {
@@ -28,6 +36,24 @@ export default {
     TheAnimal,
   },
   inject: ["animals"],
+  methods: {
+    handleScroll() {
+      if (
+        document.body.scrollTop > 200 ||
+        document.documentElement.scrollTop > 200
+      ) {
+        this.isShown = true;
+      } else {
+        this.isShown = false;
+      }
+    },
+    backToTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
 };
 </script>
 
@@ -36,6 +62,27 @@ div {
   display: flex;
   flex-wrap: wrap;
   margin: 20px;
+}
+
+button {
+  background-color: #e84135;
+  border: none;
+  padding: 20px;
+  border-radius: 50%;
+  position: fixed;
+  bottom: 125px;
+  right: 60px;
+  z-index: 999;
+  cursor: pointer;
+  display: none;
+}
+
+.show {
+  display: block;
+}
+
+button:hover {
+  color: white;
 }
 
 @media (max-width: 768px) {
